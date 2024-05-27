4f5c1da7c198f877b4e984d64fa1d58cf6833e93
theodorik comlan <comlantheodorik@gmail.com>
2024-05-27 22:36:42 +0200

    validate collaboration user-project

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,4 +1,8 @@
 class Collaboration < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+  validate do
+    errors.add(:user, "cannot be the owner of the project") if project.user == user
+  end
 end
