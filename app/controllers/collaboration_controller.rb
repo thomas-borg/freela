4691c2dbd965a95b776e193740c80f3fe50fd5df
theodorik comlan <comlantheodorik@gmail.com>
2024-05-27 17:02:48 +0200

    collaboration controller on progress

---
+++ app/controllers/collaboration_controller.rb
@@ -0,0 +1,16 @@
+class CollaborationController < ApplicationController
+  # def create
+  #   @project = Project.find(params[:project_id])
+  #   @user = Project.find(params[:user_id])
+  #   @collaboration = Collaboration.new(collaboration_params)
+  #   @collaboration.project = @project
+  #   @collaboration.user = @user
+  #   @collaboration.save
+  # end
+
+  # private
+
+  # def collaboration_params
+  #   params.require(:collaboration).permit(:project)
+  # end
+end
