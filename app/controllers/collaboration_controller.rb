f01ada5511debcd4691da75adf74bef3ff658b37
Thomas Borg <thomaslborg7@gmail.com>
2024-05-29 16:11:58 +0200

    work mothertrucker work

--- app/controllers/collaboration_controller.rb
+++ app/controllers/collaboration_controller.rb
@@ -1,14 +1,19 @@
 class CollaborationController < ApplicationController
-  # def create
-  #   @project = Project.find(params[:project_id])
-  #   @user = Project.find(params[:user_id])
-  #   @collaboration = Collaboration.new(collaboration_params)
-  #   @collaboration.project = @project
-  #   @collaboration.user = @user
-  #   @collaboration.save
-  # end
+  def new
+    @collaboration = @project.collaboration.new
+  end
 
-  # private
+  def create
+    @project = Project.find(params[:project_id])
+    @collaboration = @project.collaboration.new
+    @collaboration.project = @project
+    @collaboration.user = current_user
+    @collaboration.save
+
+    redirect_to dashboard_path
+  end
+
+  private
 
   # def collaboration_params
   #   params.require(:collaboration).permit(:project)
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
 
+  has_many :collaborations
   validates :name, presence: true
   validates :description, presence: true
-
 end
