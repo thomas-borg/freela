01641491fcea36464cc16d951c0b8acc2244c3cc
Thomas Borg <thomaslborg7@gmail.com>
2024-05-30 17:40:34 +0200

    Updated maximum characters

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,6 @@ class Project < ApplicationRecord
 
   has_many :collaborations
   validates :name, presence: true
-  validates :description, presence: true, length: {minimum: 60, maximum: 240}
+  validates :description, presence: true, length: {minimum: 60, maximum: 960}
   validates :skill, presence: true, inclusion: { in: ['Front-End Developer', 'Back-End Developer', 'Full Stack Developer', 'UX/UI Designer', 'Graphic Designer'] }
 end
