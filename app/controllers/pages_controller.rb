6161ec92c0629ea83dcc4b4d94fd4be797f8b9b4
Thomas Borg <thomaslborg7@gmail.com>
2024-05-31 14:57:03 +0200

    Made home, index, and show pages visible without signing in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home]
 
   def home
     @projects = Project.order(created_at: :desc).limit(3)
