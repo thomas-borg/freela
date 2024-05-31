56433978d2866869dcab4149c5816589ffac9660
Thomas Borg <thomaslborg7@gmail.com>
2024-05-31 14:46:34 +0200

    Updated user model and sign up, sing in, and update account views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,4 +21,6 @@ Rails.application.routes.draw do
 
 
   get 'dashboard', to: 'pages#dashboard'
+
+
 end
